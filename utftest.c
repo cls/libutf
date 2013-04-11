1059e1d8e0f02998db6100247147e6e8b5b16c40
Connor Lane Smith <cls@lubutu.com>
2013-04-11 14:14:56 +0100

    utftest: read from args

--- utftest.c
+++ utftest.c
@@ -1,39 +1,60 @@
 /* See LICENSE file for copyright and license details. */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "utf.h"
 
+static void utftest(FILE *, const char *);
+
 int
-main(void)
+main(int argc, char *argv[])
+{
+	int i;
+	FILE *fp;
+
+	if(argc <= 1)
+		utftest(stdin, "stdin");
+	else for(i = 1; i < argc; i++) {
+		if(!(fp = fopen(argv[i], "r"))) {
+			perror(argv[i]);
+			exit(1);
+		}
+		utftest(fp, argv[i]);
+		fclose(fp);
+	}
+	return 0;
+}
+
+void
+utftest(FILE *fp, const char *s)
 {
 	char buf[BUFSIZ], rbuf[UTFmax];
 	int len, len2;
 	size_t i, j, n;
 	Rune r;
 
-	for(i = 0; (n = fread(&buf[i], 1, sizeof buf - i, stdin)); i = n-i) {
+	for(i = 0; (n = fread(&buf[i], 1, sizeof buf - i, fp)); i = n-i) {
 		for(n += i, i = 0; (len = charntorune(&r, &buf[i], n-i)); i += len) {
 			if(r == Runeerror) {
-				fputs("error converting char to rune:", stderr);
+				fputs("decoding error:", stderr);
 				for(j = i; j < i+len; j++)
 					fprintf(stderr, " %02X", (unsigned char)buf[j]);
 				fputc('\n', stderr);
 			}
 			if(!(len2 = runetochar(rbuf, &r))) {
-				fprintf(stderr, "error converting rune to char: U+%02X\n", r);
+				fprintf(stderr, "encoding error: U+%02X\n", r);
 				continue;
 			}
 			else if(!fwrite(rbuf, len2, 1, stdout)) {
-				perror("write error");
-				return 1;
+				perror(s);
+				exit(1);
 			}
 		}
 		if(i < n)
 			memcpy(buf, &buf[i], n-i);
 	}
-	if(ferror(stdin)) {
-		perror("read error");
-		return 1;
+	if(ferror(stdout)) {
+		perror("stdout");
+		exit(1);
 	}
-	return 0;
 }
