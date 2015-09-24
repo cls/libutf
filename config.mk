8c0188a9d296e3a5667033b61bf046734a361e52
Geoff Nixon <geoff@geoff.codes>
2015-09-23 19:22:06 -0700

    Update UnicodeData.txt from Unicode 8.0.0 UCD.

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@
 
 PREFIX = /usr/local
 
-UNICODE = 7.0.0
+UNICODE = 8.0.0
 
 # If -std flag is set to c11, and UTF-32 is supported, then Rune = char32_t
 CFLAGS  = -pedantic -std=c89 -Iinclude -Os -Wall -Wextra
