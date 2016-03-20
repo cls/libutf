aacd8e45f559af01e94ab76d3baa1ca7d15a3545
Connor Lane Smith <cls@lubutu.com>
2016-03-20 14:15:40 +0000

    fix test suite

--- test/kosme.c
+++ test/kosme.c
@@ -10,8 +10,12 @@ main(void)
 	plan(7);
 
 	if(is(utflen(KOSME), 5, "\""KOSME"\" is 5 runes long")) {
-		Rune rbuf[6];
-		utftorunestr(rbuf, KOSME);
+		Rune rbuf[6], *p;
+		const char *s;
+		int n;
+
+		for(p = rbuf, s = KOSME; (n = chartorune(p, s)), *p != 0; p++, s += n)
+			;
 
 		is(rbuf[0], 0x03BA, "rune 0 of \""KOSME"\" is U+03BA GREEK SMALL LETTER KAPPA");
 		is(rbuf[1], 0x1F79, "rune 1 of \""KOSME"\" is U+1F79 GREEK SMALL LETTER OMICRON WITH OXIA");
