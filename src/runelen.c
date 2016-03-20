06494abaa58f8c60b1ec91b5935b3140334474d1
Connor Lane Smith <cls@lubutu.com>
2016-03-20 18:46:41 +0000

    remove runetochar length cap

--- src/runelen.c
+++ src/runelen.c
@@ -4,31 +4,25 @@
 int
 runelen(Rune r)
 {
-	if(r <= 0x7F)
+	int i;
+
+	if(r < Runeself)
 		return 1;
-	else if(r <= 0x07FF)
-		return 2;
-	else if(r <= 0xD7FF)
-		return 3;
-	else if(r <= 0xDFFF)
-		return 0; /* surrogate character */
-	else if(r <= 0xFFFD)
-		return 3;
-	else if(r <= 0xFFFF)
-		return 0; /* illegal character */
-	else if(r <= Runemax)
-		return 4;
-	else
-		return 0; /* rune too large */
+
+	for(i = 2; r > 0x07FF; i++)
+		r >>= 5;
+
+	return i;
 }
 
 size_t
 runenlen(const Rune *p, size_t len)
 {
-	size_t i, n = 0;
+	size_t i, k, n = 0;
 
 	for(i = 0; i < len; i++)
-		n += runelen(p[i]);
+		if((k = runelen(p[i])) < UTFmax)
+			n += k;
 
 	return n;
 }
--- src/runetochar.c
+++ src/runetochar.c
@@ -4,28 +4,25 @@
 int
 runetochar(char *s, const Rune *p)
 {
+	int i, n;
+	unsigned char x;
 	Rune r = *p;
 
-	switch(runelen(r)) {
-	case 1: /* 0aaaaaaa */
+	n = runelen(r);
+
+	if(n == 1) {
 		s[0] = r;
 		return 1;
-	case 2: /* 00000aaa aabbbbbb */
-		s[0] = 0xC0 | ((r & 0x0007C0) >>  6); /* 110aaaaa */
-		s[1] = 0x80 |  (r & 0x00003F);        /* 10bbbbbb */
-		return 2;
-	case 3: /* aaaabbbb bbcccccc */
-		s[0] = 0xE0 | ((r & 0x00F000) >> 12); /* 1110aaaa */
-		s[1] = 0x80 | ((r & 0x000FC0) >>  6); /* 10bbbbbb */
-		s[2] = 0x80 |  (r & 0x00003F);        /* 10cccccc */
-		return 3;
-	case 4: /* 000aaabb bbbbcccc ccdddddd */
-		s[0] = 0xF0 | ((r & 0x1C0000) >> 18); /* 11110aaa */
-		s[1] = 0x80 | ((r & 0x03F000) >> 12); /* 10bbbbbb */
-		s[2] = 0x80 | ((r & 0x000FC0) >>  6); /* 10cccccc */
-		s[3] = 0x80 |  (r & 0x00003F);        /* 10dddddd */
-		return 4;
-	default:
-		return 0; /* error */
 	}
+
+	if(n > UTFmax)
+		return 0;
+
+	for(i = n; --i > 0; r >>= 6)
+		s[i] = 0x80 | (r & 0x3F);
+
+	x = 0xFF >> n;
+	s[0] = ~x | r;
+
+	return n;
 }
