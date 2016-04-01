ce4eaaccf07b6ec00d9f8bc4395c50b7ea63741c
Connor Lane Smith <cls@lubutu.com>
2016-04-01 14:08:35 +0100

    runelen: simplify and explain

--- src/runelen.c
+++ src/runelen.c
@@ -12,8 +12,8 @@ runelen(Rune r)
 	if(r < Runeself)
 		return 1;
 
-	for(i = 2; r > 0x07FF; i++)
-		r >>= 5;
+	for(i = 2; r > 03777; i++) /* too big to fit in this many (i) utf bytes */
+		r >>= 5; /* gain 6 bits from continuation byte but lose 1 in leader */
 
 	return i;
 }
@@ -21,11 +21,10 @@ runelen(Rune r)
 size_t
 runenlen(const Rune *p, size_t len)
 {
-	size_t i, k, n = 0;
+	size_t i, k = 0;
 
 	for(i = 0; i < len; i++)
-		if((k = runelen(p[i])) < UTFmax)
-			n += k;
+		k += runelen(*p++);
 
-	return n;
+	return k;
 }
